fd1554b3582b87901e471732759f619da9d1935a
timothe59 <timothe.ruc@gmail.com>
2016-09-23 11:11:06 +0200

    tp1

---
+++ tp1/tp1_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "tp1_2.h"
+
+void fonction_b(int param);
+
+int main(void)
+{
+	fonction_b (0);
+	fonction_a (20);
+	return 0 ;
+
+}
---
+++ tp1/tp1_2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int fonction_a(int param)
+{
+return  42+ param;
+}
+
+void fonction_b(int param);
